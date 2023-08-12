b331fc107707dbc1c644a3b3d20194baca8d3223
Tebogo Lefatola <tebogo@aluvesqe.co.za>
2023-08-12 05:53:07 +0200

    --

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+N
+src/main/java/dto/BookDTO.java,5\5\55b76835ae707082004eb88566000a3b82439b85
+S
+#src/main/java/utils/RestHelper.java,2\8\2820b00b6d675b7594a1596e5d8f16568a7af58d
--- src/test/java/RunCucumberIT.java
+++ src/test/java/RunCucumberIT.java
@@ -5,11 +5,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/feature",
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "json:target/cucumber-reports/cucumber.json"},
-        tags="@run-this"
+        tags="@run-this",
+        glue = "steps"
 )
-
 public class RunCucumberIT {
 
 }
